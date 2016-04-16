15c4b12e014fdf811720a1a8bdb56b51d676920e
yujiangshui <yujiangshui@gmail.com>
2016-04-16 22:56:45 +0800

    Make ES6+ Portal

--- Libraries/Portal/Portal.js
+++ Libraries/Portal/Portal.js
@@ -14,6 +14,7 @@ import React from 'react';
 import Platform from 'ReactPlatform';
 import StyleSheet from 'ReactStyleSheet';
 import View from 'ReactView';
+import autobind from 'autobind-decorator';
 
 var _portalRef: any;
 
@@ -32,15 +33,15 @@ var lastUsedTag = 0;
  * Never use `<Portal>` in your code. There is only one Portal instance rendered
  * by the top-level `renderApplication`.
  */
-var Portal = React.createClass({
-  statics: {
+class Portal extends React.Component {
+  static statics = {
     /**
      * Use this to create a new unique tag for your component that renders
      * modals. A good place to allocate a tag is in `componentWillMount`
      * of your component.
      * See `showModal` and `closeModal`.
      */
-    allocateTag: function(): string {
+    allocateTag(): string {
       return '__modal_' + (++lastUsedTag);
     },
 
@@ -50,7 +51,7 @@ var Portal = React.createClass({
      * This tag can be later used in `closeModal`.
      * @param component A React component to be rendered.
      */
-    showModal: function(tag: string, component: any) {
+    showModal(tag: string, component: any) {
       if (!_portalRef) {
         console.error('Calling showModal but no Portal has been rendered.');
         return;
@@ -63,7 +64,7 @@ var Portal = React.createClass({
      * @param tag A unique tag identifying the React component to remove.
      * Must exactly match the tag previously passed to `showModal`.
      */
-    closeModal: function(tag: string) {
+    closeModal(tag: string) {
       if (!_portalRef) {
         console.error('Calling closeModal but no Portal has been rendered.');
         return;
@@ -74,20 +75,18 @@ var Portal = React.createClass({
     /**
      * Get an array of all the open modals, as identified by their tag string.
      */
-    getOpenModals: function(): Array<string> {
+    getOpenModals(): Array<string> {
       if (!_portalRef) {
         console.error('Calling getOpenModals but no Portal has been rendered.');
         return [];
       }
       return _portalRef._getOpenModals();
     },
-  },
+  }
 
-  getInitialState: function() {
-    return {modals: {}};
-  },
+  state = {modals: {}}
 
-  _showModal: function(tag: string, component: any) {
+  _showModal(tag: string, component: any) {
     // We are about to open first modal, so Portal will appear.
     // Let's disable accessibility for background view on Android.
     if (this._getOpenModals().length === 0 && this.props.onModalVisibilityChanged) {
@@ -100,9 +99,9 @@ var Portal = React.createClass({
       modals[tag] = component;
       return {modals};
     });
-  },
+  }
 
-  _closeModal: function(tag: string) {
+  _closeModal(tag: string) {
     if (!this.state.modals.hasOwnProperty(tag)) {
       return;
     }
@@ -118,13 +117,13 @@ var Portal = React.createClass({
       delete modals[tag];
       return {modals};
     });
-  },
+  }
 
-  _getOpenModals: function(): Array<string> {
+  _getOpenModals(): Array<string> {
     return Object.keys(this.state.modals);
-  },
+  }
 
-  render: function() {
+  render() {
     _portalRef = this;
     if (!this.state.modals) {
       return null;
@@ -143,7 +142,8 @@ var Portal = React.createClass({
       </View>
     );
   }
-});
+
+};
 
 var styles = StyleSheet.create({
   modalsContainer: {
@@ -156,5 +156,7 @@ var styles = StyleSheet.create({
 });
 
 Portal.isReactNativeComponent = true;
+
+autobind(Portal);
 
 export default Portal;
