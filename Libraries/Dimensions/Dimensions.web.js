51e1b653d5bdda032e3ee885687a67931272c783
Mark Vayngrib <mark@tradle.io>
2016-10-02 00:37:16 -0400

    return live dimensions (vs cached)

--- Libraries/Dimensions/Dimensions.web.js
+++ Libraries/Dimensions/Dimensions.web.js
@@ -9,9 +9,9 @@
 let dimensions = {
   // Not map to real window size, because that map to screen size in native env.
   window: {
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight,
-    scale: window.devicePixelRatio || 1,
+    get width() { return document.documentElement.clientWidth },
+    get height() { return document.documentElement.clientHeight },
+    get scale() { return window.devicePixelRatio || 1 },
   },
   modalFullscreenView: {
     width: screen.width,
