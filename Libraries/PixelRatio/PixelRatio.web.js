27e81444662a3240ef2a0a374bd6c93e3315964a
kunkun <baokundu@gmail.com>
2017-02-08 18:52:12 +0800

    PixelRatio 增加 roundToNearestPixel 方法

--- Libraries/PixelRatio/PixelRatio.web.js
+++ Libraries/PixelRatio/PixelRatio.web.js
@@ -89,6 +89,17 @@ class PixelRatio {
     return Math.round(layoutSize * PixelRatio.get());
   }
 
+ /**
+   * Rounds a layout size (dp) to the nearest layout size that corresponds to
+   * an integer number of pixels. For example, on a device with a PixelRatio
+   * of 3, `PixelRatio.roundToNearestPixel(8.4) = 8.33`, which corresponds to
+   * exactly (8.33 * 3) = 25 pixels.
+   */
+  static roundToNearestPixel(layoutSize: number): number {
+    var ratio = PixelRatio.get();
+    return Math.round(layoutSize * ratio) / ratio;
+  }
+
   // No-op for iOS, but used on the web. Should not be documented.
   static startDetecting() {}
 }
