c92bdbb5268576571dcc72bd36dfe76e2d3a0024
Li Zheng <flyskywhy@gmail.com>
2017-08-31 16:33:56 +0800

    Support U4 core UC Browser

--- Libraries/StyleSheet/extendProperties.web.js
+++ Libraries/StyleSheet/extendProperties.web.js
@@ -72,6 +72,7 @@ else flexboxSpec = '2009';
 var isUCBrowser = /UCBrowser/i.test(navigator.userAgent);
 // only U3 core need 2009 spec, and only this way can detect another core
 var notU3 = /UCBS/i.test(navigator.userAgent);
+notU3 = !(/U3/i.test(navigator.userAgent));
 if (isUCBrowser && !notU3) flexboxSpec = '2009';
 
 const isIE = /Trident/i.test(navigator.userAgent);
